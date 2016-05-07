8e7d592587d2d815e0f2f23421210a2d8d7aa62e
Brian Wong <wongbm2@uci.edu>
2016-05-06 23:37:56 -0700

    lab3

---
+++ lab3.c
@@ -0,0 +1,28 @@
+//Wong, Brian: 34216498
+//Cha, Yoon Ho: 32075491
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+
+	char line[80];
+	char* commands[50];        //command parameters for execvp
+	int status = 0;
+
+	while(strcmp(line,"quit")!=0){
+		printf(">");
+
+		//printf("new command is here");
+        scanf("%s",line);
+    }
+
+
+
+
+
+ return 0;
+}
