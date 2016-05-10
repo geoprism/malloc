a507df0681a77552d0c379b17ec0eb503b520c55
Jason <silentspeedz@gmail.com>
2016-05-09 20:09:47 -0700

    Starting Point

---
+++ lab3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+	{
+		char* ptr_one;
+
+
+
+		while(1){
+			
+		}
+
+		ptr_one = (char *)malloc(400);
+
+
+
+		int i;
+
+		int j;
+
+		for(j = 0; j<400; j++){
+			printf("%p\n", ptr_one);
+			ptr_one++;
+		}
+
+
+		return 0;
+	}
